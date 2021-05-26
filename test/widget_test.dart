9d074f64dbf30e808a321f6b35e25597230852e3
Aditi Atul Vaidya <61463717+AditiTheCoder@users.noreply.github.com>
2021-05-26 19:01:28 +0530

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,32 @@
+// This is a basic Flutter widget test.
+// Project Created By Aditi Vaidya
+// Email ID :- aditivaidya10@gmail.com
+// LinkedIn Profile :- https://www.linkedin.com/in/aditi-vaidya-7a3451136/
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:calvertor/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(CalcApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
